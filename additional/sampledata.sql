7a845212f2346e73fe5e42e2955ce809c5947ffd
WebMechanic <serradeil@webmechanic.biz>
2012-08-26 20:02:57 +0200

    MyThings: Komponente - initialer commit. Kapitel 3 Startskripte (sql, xml)

---
+++ additional/sampledata.sql
@@ -0,0 +1,38 @@
+###################################################################
+# Tabelle #__mythings für die Komponente com_mythings
+# Buch "Joomla! 2.5 - Erweiterungen programmieren"
+#
+# Beispieldaten für Kapitel '4 - 17'
+#
+###################################################################
+
+TRUNCATE `#__mythings`;
+INSERT INTO `#__mythings` (`owner`, `category`, `title`, `description`, `state`, `value`, `weight`, `lent_by`, `lent`) VALUES
+('René', 'Diverses', 'Kuschelteddy', 'Nur an nearest and dearest zu verleihen. ', 'älter', 'unschätzbar', '200g', 'Geheim', '2011-09-21 00:00:00'),
+('Christiane', 'Diverses', 'Innerer Schweinehund', 'Der stärkste ISH der Welt. Schützt zuverlässig vor allen ungeliebten Tätigkeiten. ', '', 'unschätzbar', 'normal', '', '0000-00-00 00:00:00'),
+('Axel', 'Elektronik', 'Webcam', ' ', 'neu', '79.-', '', '', '0000-00-00 00:00:00'),
+('Christiane', 'Kleidung', 'Dirndl Grün + Schwarz', 'Nicht während des Oktoberfests ausleihbar. Körpergröße 158cm', 'fast neu', '1.250,-', 'normal', '', '0000-00-00 00:00:00'),
+('Axel', 'Geschirr', 'Kaffeemaschine', 'Bis zu 10 Tassen, Bedienungsanleitung deutsch, ermöglicht ausgedehnte Nachtarbeit ', 'neu', '', '2kg', 'René', '2011-05-10 00:00:00'),
+('Julian', 'Fachbuch', 'Ein Mann, ein Buch', 'Lebensrettende Ratschläge, zum Beispiel: Wie man eine Boeing landet, wie man eine Krawatte bindet, wie man eine Frau rumkriegt', 'neu', '', '', 'Christiane', '2011-05-10 00:00:00'),
+('Christiane', 'Elektronik', 'Scanner', 'Canon, 9494XS', '', '120,-', '2 kg', '', '0000-00-00 00:00:00'),
+('Axel', 'Kleidung', 'Abendanzug', 'Komplett mit Lackschuhen und Kummerbund', 'neu', '1200,-', '', '', '0000-00-00 00:00:00'),
+('Christiane', 'Schmuck', 'Armbanduhr', 'Lacroix mit Lederband', 'fast neu', '1.250,-', '', 'Eva', '0000-00-00 00:00:00'),
+('Axel', 'Diverses', 'Panther', 'Schwarzer Steiff-Panther, wirkt entspannend', 'neu', '', '2kg', 'René', '2011-05-10 00:00:00'),
+('Julian', 'Buch', 'DAEMON', 'Science Fiction, spannend bis zu letzten Zeile', 'neu', '', '', 'Christiane', '2012-02-10 00:00:00'),
+('Axel', 'Auto', 'Wohnwagen für Fernreisen', 'Maximal 3 Personen, wenn sie nicht zu groß sind', 'neu', '', '', '', '0000-00-00 00:00:00'),
+('Axel', 'Deko', 'Blumentopf', 'Durchmesser 30 cm', 'neu', '', '', '', '0000-00-00 00:00:00'),
+('Christiane', 'Elektronik', 'Digitalkamera', 'Canon X34', 'gut', '420,-', '', '', '0000-00-00 00:00:00'),
+('René', 'Geschirr', 'Kaffeetasse', 'mit BOFH-Aufdruck, für Nachtarbeiter', 'neu', '', '', 'Christiane', '2012-02-27 00:00:00'),
+('Axel', 'Kleidung', 'Panamahut', 'Original, aus Peru', 'neu', '', '', '', '0000-00-00 00:00:00'),
+('Christiane', 'Deko', 'Kuschelpanther', 'Schwarz, sehr weich. 80 cm lang', 'gebraucht', '', '', '', '0000-00-00 00:00:00'),
+('René', 'Deko', 'Bronzebuddha', 'Japanischer Bronze-Größe ca. 40 cm.  ', '', '3000,-', '15kg', '', '0000-00-00 00:00:00'),
+('Axel', 'Schmuck', 'Armbanduhr', 'Klassische Uhr,  Lederband', 'getragen', '370,-', '', '', '0000-00-00 00:00:00'),
+('Christiane', 'Schmuck', 'Diadem', 'Für besondere Gelegenheiten', '', '20.000,-', '', 'Eva', '2011-06-11 00:00:00'),
+('Christiane', 'Auto', 'Smart', 'leicht einzuparken', 'neu', '', '', '', '0000-00-00 00:00:00'),
+('René', 'Buch', 'DAEMON', 'SciFi, spannend bis zur letzten Zeile ', 'neu', '', '', '', '0000-00-00 00:00:00'),
+('Christiane', 'Deko', 'Bodenvase', 'Höhe 90 cm', 'angeschlagen', '', '', '', '0000-00-00 00:00:00'),
+('René', 'Deko', 'Teppich', 'Persien, 1900', 'uralt', '', '12 kg', '', '0000-00-00 00:00:00'),
+('Axel', 'Haushalt', 'Küchenmesser', 'sehr, sehr scharf, dünne Klinge', 'gebraucht', '', '', '', '0000-00-00 00:00:00'),
+('Christiane', 'Kleidung', 'Panamahut', 'Original, aus Peru', 'neu', '', '', '', '0000-00-00 00:00:00'),
+('René', 'Deko', 'Sonnenbrille', 'Ray Ban', 'neu', '', '', '', '0000-00-00 00:00:00'),
+('Axel', 'Buch', 'Joomla 2.5', 'sh. Amazon', 'neu', '', '', '', '0000-00-00 00:00:00');
---
+++ com_mythings/admin/sql/install.sql
@@ -0,0 +1,21 @@
+#
+# Tabelle `#__mythings`
+# Kapitel 3 (Start)
+# Vorbereitung und Erstinstallation
+#
+
+DROP TABLE IF EXISTS `#__mythings`;
+CREATE TABLE IF NOT EXISTS `#__mythings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` varchar(50) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `description` text,
+  `state` varchar(50) DEFAULT NULL,
+  `value` varchar(12) DEFAULT NULL,
+  `weight` varchar(12) DEFAULT NULL,
+  `lent_by` varchar(50) DEFAULT NULL,
+  `lent` datetime DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
